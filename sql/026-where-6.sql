21750601e56de7927b173aeb32b92cfe3f9d73df
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-11-24 19:07:59 +0200

    sql/026: completed

---
+++ sql/026-where-6.sql
@@ -0,0 +1,9 @@
+-- Author: Anton Kucherenko anton.kucherenko@tuni.fi
+-- Date: 2003-11-24
+-- File: 026-where-6.sql
+
+SELECT ename, deptno
+FROM emp
+WHERE deptno = 10 OR deptno = 30;
+
+-- End of file
