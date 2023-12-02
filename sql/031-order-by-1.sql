1cf683b9460aa42d8872cd4c6e1f4191b08e1c0e
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-02 12:16:58 +0200

    sql/031: completed

---
+++ sql/031-order-by-1.sql
@@ -0,0 +1,10 @@
+-- Author: Anton Kucherenko anton.kucherenko@tuni.fi
+-- Date: 2003-12-02
+-- File: 031-order-by-1.sql
+
+SELECT ename, sal, sal * 1.15 AS "new salary"
+FROM emp
+WHERE mgr = (SELECT empno FROM emp WHERE ename = 'BLAKE')
+ORDER BY "new salary", ename;
+
+-- End of file
