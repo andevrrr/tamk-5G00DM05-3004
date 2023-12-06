38f233f38cefee41212695ff86d5ef70ad5cf4e7
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-06 14:34:41 +0300

    sql/032: completed

--- sql/032-order-by-2.sql
+++ sql/032-order-by-2.sql
@@ -2,9 +2,13 @@
 -- Date: 2003-12-02
 -- File: 032-order-by-2.sql
 
-SELECT empno, ename, deptno, hiredate
+SELECT empno,
+    ename,
+    deptno,
+    hiredate
 FROM emp
-WHERE empno > 7900 OR empno < 7600
+WHERE empno > 7900
+    OR empno < 7600
 ORDER BY empno;
 
 -- End of file
