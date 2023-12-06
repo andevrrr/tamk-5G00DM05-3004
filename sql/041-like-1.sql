767e8ecf65c01166edebda0c19b9162708e0976f
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-06 14:41:50 +0300

    sql/041: completed

---
+++ sql/041-like-1.sql
@@ -0,0 +1,12 @@
+-- Author: Anton Kucherenko anton.kucherenko@tuni.fi
+-- Date: 2003-12-06
+-- File: 041-like-1.sql
+
+SELECT
+    ename
+FROM
+    emp
+WHERE
+    ename LIKE '__a%';
+
+-- End of file
