4c97c36666246c79842b4d0136ca61fada49584d
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 19:24:42 +0300

    052/sql: completed

---
+++ sql/052-case-2.sql
@@ -0,0 +1,17 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-11
+-- File: 052-case-2.sql
+
+SELECT ename,
+       CASE
+           WHEN deptno = 30 THEN
+               CASE
+                   WHEN comm IS NULL THEN 100
+                   ELSE comm * 1.05
+               END
+           ELSE comm
+       END AS comm
+FROM emp
+ORDER BY comm, ename;
+
+-- End of file
