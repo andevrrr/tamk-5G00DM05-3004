203e517c45241a9f898bcf8e87ed4f174d60b675
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 15:05:49 +0300

    sql/015: format errors fixed

--- sql/015-query-3.sql
+++ sql/015-query-3.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-11-21
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-11-21
 -- File: 015-query-3.sql
 
 SELECT DISTINCT job
