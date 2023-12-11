43c22699c76f954b869ca17487d74e01854b81d0
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 15:06:53 +0300

    sql/023: format errors fixed

--- sql/023-where-3.sql
+++ sql/023-where-3.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-11-24
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-11-24
 -- File: 023-where-3.sql
 
 SELECT ename, sal
