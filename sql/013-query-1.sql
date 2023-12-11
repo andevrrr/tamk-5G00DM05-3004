cad922c02558ee4e8139f317003b970767e59f31
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 15:05:20 +0300

    sql/013: format errors fixed

--- sql/013-query-1.sql
+++ sql/013-query-1.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-11-21
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-11-21
 -- File: 013-query-1.sql
 
 SELECT ename, job, hiredate, empno FROM emp;
