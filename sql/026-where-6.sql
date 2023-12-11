606e6e0f8d8b5da846625b775a755ebac64aad14
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 15:07:36 +0300

    sql/026: format errors fixed

--- sql/026-where-6.sql
+++ sql/026-where-6.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-11-24
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-11-24
 -- File: 026-where-6.sql
 
 SELECT ename, deptno
