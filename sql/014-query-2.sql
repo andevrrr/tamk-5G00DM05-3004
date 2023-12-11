2faaaf1f54332cd4a6a13a7180d685c4e2cb9ec4
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 15:05:36 +0300

    sql/014: format errors fixed

--- sql/014-query-2.sql
+++ sql/014-query-2.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-11-21
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-11-21
 -- File: 014-query-2.sql
 
 SELECT ename,
