cde1de04347cd717d39c178a1fff242676c6652a
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 16:55:14 +0300

    sql/031: format errors fixed

--- sql/031-order-by-1.sql
+++ sql/031-order-by-1.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-12-02
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-02
 -- File: 031-order-by-1.sql
 
 SELECT ename, sal, sal * 1.15 AS "new salary"
