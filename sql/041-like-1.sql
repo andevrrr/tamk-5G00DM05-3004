50b9512dcfa7a76cd13202e0d64bdfae83c661b4
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-11 16:56:43 +0300

    sql/041: format errors fixed

--- sql/041-like-1.sql
+++ sql/041-like-1.sql
@@ -1,5 +1,5 @@
--- Author: Anton Kucherenko anton.kucherenko@tuni.fi
--- Date: 2003-12-06
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-06
 -- File: 041-like-1.sql
 
 SELECT
