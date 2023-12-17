078a045bbf619ff6c806f8e5abe38e06b88e86e3
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-17 23:43:38 +0300

    sql/074: completed

---
+++ sql/074-function-4.sql
@@ -0,0 +1,15 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-17
+-- File: 074-function-4.sql
+
+SELECT
+    ename,
+    CASE
+        WHEN SUBSTR(ename, 1, 1) IN ('A', 'M', 'J') THEN LOWER(job)
+        ELSE job
+    END AS job
+FROM emp
+ORDER BY ename, job;
+
+
+-- End of file
