8a83653ffc04b515cf38877caac1f8ad758bcf27
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-17 23:36:44 +0300

    sql/071: completed

---
+++ sql/071-functions-1.sql
@@ -0,0 +1,9 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-17
+-- File: 071-function-1.sql
+
+SELECT LOWER(ename) AS Name, LOWER(job) AS Job, sal AS Salary
+FROM emp
+ORDER BY Name, Job;
+
+-- End of file
