d4b63e7183a6db4cbbd7c467076ffec234f76474
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-17 23:38:40 +0300

    sql/072: completed

---
+++ sql/072-function-2.sql
@@ -0,0 +1,9 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-17
+-- File: 072-function-2.sql
+
+SELECT ename AS Name, LENGTH(ename) AS Characters
+FROM emp
+ORDER BY Characters, Name;
+
+-- End of file
