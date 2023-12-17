bf3b65602eef3cef727b8dabd365f6415a92916a
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-17 23:40:31 +0300

    sql/073: completed

---
+++ sql/073-function-3.sql
@@ -0,0 +1,13 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-17
+-- File: 073-function-3.sql
+
+SELECT
+    empno,
+    ename,
+    ROUND(sal * 1.15) AS "new salary",
+    ROUND(sal * 1.15) - sal AS increase
+FROM emp
+ORDER BY ename, "new salary";
+
+-- End of file
