6f9f0d4bc32697d03abd1df1da354028f5cc27de
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-22 19:58:11 +0300

    sql/082: completed

---
+++ sql/082-group-functions-2.sql
@@ -0,0 +1,9 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-22
+-- File: 082-group-functions-2.sql
+
+SELECT COUNT(*) AS "count of dept 30"
+FROM emp
+WHERE deptno = 30;
+
+-- End of file
