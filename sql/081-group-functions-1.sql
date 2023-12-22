42f6f0f929d10fb0e5af898de1bb25f7e5f6cc4d
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-22 19:53:29 +0300

    sql/081: completed

---
+++ sql/081-group-functions-1.sql
@@ -0,0 +1,10 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-22
+-- File: 081-group-functions-1.sql
+
+SELECT job, COUNT(*) AS "count of managers"
+FROM emp
+WHERE job IN ('MANAGER', 'PRESIDENT')
+GROUP BY job;
+
+-- End of file
