c80e442ea0a3fea233fde210fd75c30b3e0ca885
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-24 21:27:08 +0300

    sql/093: completed

---
+++ sql/093-multiple-tables-3.sql
@@ -0,0 +1,11 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-24
+-- File: 093-multiple-tables-3.sql
+
+SELECT e.ename, d.dname, d.loc
+FROM emp e
+JOIN dept d ON e.deptno = d.deptno
+WHERE e.comm IS NOT NULL AND e.comm > 0
+ORDER BY e.ename;
+
+-- End of file
