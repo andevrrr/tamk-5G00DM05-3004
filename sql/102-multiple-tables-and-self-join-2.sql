171cdc8f3058e37ccd50a33122cd42a6b2122400
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-27 23:54:31 +0400

    sql/102: completed

---
+++ sql/102-multiple-tables-and-self-join-2.sql
@@ -0,0 +1,18 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-27
+-- File: 102-multiple-tables-and-self-join-2.sql
+
+SELECT
+    e.ename AS "Employee",
+    e.empno AS "Emp#",
+    m.ename AS "Manager",
+    m.empno AS "Mgr#"
+FROM
+    emp e
+    INNER JOIN emp m ON e.mgr = m.empno
+WHERE
+    m.ename IN ('BLAKE', 'FORD', 'SCOTT')
+ORDER BY
+    "Manager", "Employee";
+
+-- End of file
