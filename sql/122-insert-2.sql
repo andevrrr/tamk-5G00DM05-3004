04ef2bdef422ba74baba7b99c10e8febc5f8aec5
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-31 13:29:15 +0400

    sql/122: completed

---
+++ sql/122-insert-2.sql
@@ -0,0 +1,10 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-31
+-- File: 122-insert-2.sql
+
+INSERT INTO football (id, first, last, team, comment) VALUES
+(1, 'Jim', 'Anders', 'Manchester', 'UPS 10'),
+(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+(3, 'Jenny', 'Richards', 'Highland', 'Scottish national');
+
+-- End of file
