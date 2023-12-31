45913384c1fc0d25e644bfd391d9605a2192d7ca
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-31 13:26:29 +0400

    sql/121: completed

---
+++ sql/121-insert-1.sql
@@ -0,0 +1,10 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-31
+-- File: 121-insert-1.sql
+
+INSERT INTO person (id, last, first, phone, zip, city, address) VALUES
+(1, 'Doe', 'John', '+123455', '11122', 'New York', '2th avenue'),
+(2, 'Jordan', 'Mike', '+221122', '01212', 'Washington', 'South Park 3'),
+(3, 'Durak', 'Stephen', '+550011', '55654', 'Miami', 'Sea Drive 112');
+
+-- End of file
