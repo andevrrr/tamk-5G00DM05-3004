fee86c837e68f9cde63e93d7fdc4495441039b17
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-31 13:01:50 +0400

    sql/114: completed

---
+++ sql/114-create-table-4.sql
@@ -0,0 +1,12 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-31
+-- File: 114-create-table-4.sql
+
+CREATE TABLE inventory (
+    id INT PRIMARY KEY,
+    item VARCHAR(255),
+    worth INT,
+    comment TEXT
+);
+
+-- End of file
