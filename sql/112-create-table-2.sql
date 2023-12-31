00e6e585800abd6bdb4cb8db47164aaba9d81466
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-31 12:59:08 +0400

    sql/112: completed

---
+++ sql/112-create-table-2.sql
@@ -0,0 +1,13 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-31
+-- File: 112-create-table-2.sql
+
+CREATE TABLE football (
+    id INT PRIMARY KEY,
+    first VARCHAR(255),
+    last VARCHAR(255),
+    team VARCHAR(255),
+    comment TEXT
+);
+
+-- End of file
