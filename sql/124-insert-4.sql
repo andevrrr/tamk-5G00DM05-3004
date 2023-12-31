06f082d3d8b1e65250e77e69638cb4d55b714170
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-31 13:37:49 +0400

    sql/124: completed

---
+++ sql/124-insert-4.sql
@@ -0,0 +1,10 @@
+-- Author: Anton Kucherenko <anton.kucherenko@tuni.fi>
+-- Date: 2023-12-31
+-- File: 124-insert-4.sql
+
+INSERT INTO inventory (id, item, worth, comment) VALUES
+(1, 'baseball', 150, 'My first baseball'),
+(2, 'bike', 2000, 'For mountain riding'),
+(3, 'playcards', 10, 'Funny pictures');
+
+-- End of file
