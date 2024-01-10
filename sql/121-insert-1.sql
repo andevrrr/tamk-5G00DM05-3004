538ec2f0ac56be65270a9169b62e023bd1885010
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 20:32:34 +0300

    sql/121: syntax checks fixed

--- sql/121-insert-1.sql
+++ sql/121-insert-1.sql
@@ -3,8 +3,8 @@
 -- File: 121-insert-1.sql
 
 INSERT INTO person (id, last, first, phone, zip, city, address) VALUES
-(1, 'Doe', 'John', '+123455', '11122', 'New York', '2th avenue'),
-(2, 'Jordan', 'Mike', '+221122', '01212', 'Washington', 'South Park 3'),
-(3, 'Durak', 'Stephen', '+550011', '55654', 'Miami', 'Sea Drive 112');
+(1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue'),
+(2, 'Jordan', 'Mike', '221122', '01212', 'Washington', 'South Park 3'),
+(3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112');
 
 -- End of file
