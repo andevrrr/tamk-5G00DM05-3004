6baf616ae3b87bf9c37dc07d9fa8abbe45b4da03
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 18:52:38 +0300

    sql/095: syntax checks fixed

--- sql/095-multiple-tables-5.sql
+++ sql/095-multiple-tables-5.sql
@@ -3,9 +3,9 @@
 -- File: 095-multiple-tables-5.sql
 
 SELECT e.ename, e.job, e.deptno, d.dname
-FROM emp e
-JOIN dept d ON e.deptno = d.deptno
-WHERE d.dname IN ('ACCOUNTING', 'SALES', 'OPERATIONS') AND e.sal > 1000
-ORDER BY e.ename;
+FROM emp AS e
+JOIN dept AS d ON e.deptno = d.deptno
+WHERE LOWER(d.dname) IN ('accounting', 'sales', 'operations') AND e.sal > 1000
+ORDER BY e.ename ASC;
 
 -- End of file
