62c675d5ddd2b7d5de58110cacda08668c6e7efb
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 21:05:06 +0300

    sql/114: syntax checks fixed

--- sql/114-create-table-4.sql
+++ sql/114-create-table-4.sql
@@ -3,10 +3,10 @@
 -- File: 114-create-table-4.sql
 
 CREATE TABLE inventory (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY UNIQUE NOT NULL,
     item VARCHAR(255),
     worth INT,
-    comment TEXT
+    comment VARCHAR(MAX)
 );
 
 -- End of file
