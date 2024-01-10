5850fec79be3f5bfe95612ad1b4fdfd6c74cfe45
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 19:03:07 +0300

    sql/112: syntax checks fixed

--- sql/112-create-table-2.sql
+++ sql/112-create-table-2.sql
@@ -3,7 +3,7 @@
 -- File: 112-create-table-2.sql
 
 CREATE TABLE football (
-    id INT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     first VARCHAR(255),
     last VARCHAR(255),
     team VARCHAR(255),
