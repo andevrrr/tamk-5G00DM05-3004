763180b80a50a0eedfd71ee1a2c66369d698d4f1
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 21:01:30 +0300

    sql/111: syntax checks fixed

--- sql/111-create-table-1.sql
+++ sql/111-create-table-1.sql
@@ -3,10 +3,10 @@
 -- File: 111-create-table-1.sql
 
 CREATE TABLE person (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY UNIQUE,
     last VARCHAR(255),
     first VARCHAR(255),
-    phone VARCHAR(20) CHECK (phone LIKE '+%' AND phone NOT LIKE '%[^+ 0-9]%'),
+    phone VARCHAR(20) CHECK (LOWER(phone) LIKE '+%' AND phone NOT LIKE '%[^+ 0-9]%'),
     zip VARCHAR(10),
     city VARCHAR(255),
     address VARCHAR(255)
