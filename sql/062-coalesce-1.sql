dcb0d2690caddb7735e60b5b78d0d173d9bd4b05
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 18:38:03 +0300

    sql/062: fixed

--- sql/062-coalesce-1.sql
+++ sql/062-coalesce-1.sql
@@ -3,8 +3,8 @@
 -- File: 062-coalesce-2.sql
 
 SELECT ename AS Name,
-       (sal * 0.69 + COALESCE(comm, 0) * 0.80) AS "Net Income",
-       (sal * 0.31 + COALESCE(comm, 0) * 0.20) AS "Tax"
+       (COALESCE(sal, 0) * 0.69 + COALESCE(comm, 0) * 0.80) AS "Net Income",
+       (COALESCE(sal, 0) * 0.31 + COALESCE(comm, 0) * 0.20) AS "Tax"
 FROM emp
 ORDER BY "Net Income";
 
