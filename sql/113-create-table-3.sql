ad6a45d0186e84b5cc014951fd5776d1a08b93d2
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 21:03:31 +0300

    sql/113: syntax checks fixed

--- sql/113-create-table-3.sql
+++ sql/113-create-table-3.sql
@@ -3,12 +3,12 @@
 -- File: 113-create-table-3.sql
 
 CREATE TABLE weather (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY UNIQUE,
     time_of_reading TIMESTAMP NOT NULL,
-    high DECIMAL(5, 2),
-    low DECIMAL(5, 2),
-    sig VARCHAR(2),
-    comment TEXT
+    high DECIMAL,
+    low DECIMAL,
+    sig VARCHAR(255),
+    comment VARCHAR(MAX)
 );
 
 -- End of file
