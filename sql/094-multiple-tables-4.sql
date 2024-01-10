443d715319213789be54d1ff692cbbcff2a670ed
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 18:51:59 +0300

    sql/094: syntax checks fixed

--- sql/094-multiple-tables-4.sql
+++ sql/094-multiple-tables-4.sql
@@ -3,9 +3,9 @@
 -- File: 094-multiple-tables-4.sql
 
 SELECT e.ename, d.dname
-FROM emp e
-JOIN dept d ON e.deptno = d.deptno
-WHERE e.ename LIKE '%a%' AND e.sal > 1100 AND e.sal <= 2200
-ORDER BY e.ename;
+FROM emp AS e
+JOIN dept AS d ON e.deptno = d.deptno
+WHERE LOWER(e.ename) LIKE '%a%' AND e.sal > 1100 AND e.sal <= 2200
+ORDER BY e.ename ASC;
 
 -- End of file
