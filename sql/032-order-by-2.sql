cb16937c8cb93797a0909eb8f5ff9a92497e108e
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-10 18:03:55 +0300

    sql/032: order by ASC fixed

--- sql/032-order-by-2.sql
+++ sql/032-order-by-2.sql
@@ -2,13 +2,9 @@
 -- Date: 2023-12-02
 -- File: 032-order-by-2.sql
 
-SELECT empno,
-    ename,
-    deptno,
-    hiredate
+SELECT empno, ename, deptno, hiredate
 FROM emp
-WHERE empno > 7900
-    OR empno < 7600
-ORDER BY empno;
+WHERE empno > 7900 OR empno < 7600
+ORDER BY empno ASC;
 
 -- End of file
