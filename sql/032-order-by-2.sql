761f65c70d637511f8a23d6a504e622b7d7410b4
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-11 18:12:38 +0300

    sql/032: fixed the code

--- sql/032-order-by-2.sql
+++ sql/032-order-by-2.sql
@@ -2,16 +2,9 @@
 -- Date: 2023-12-02
 -- File: 032-order-by-2.sql
 
-SELECT
-    deptno AS "Dept",
-    ename AS "Employee",
-    ROUND(sal * 0.31, 2) AS "Taxes"
-FROM
-    emp
-WHERE
-    deptno = 10
-ORDER BY
-    deptno ASC,
-    ename ASC;
+SELECT empno, ename, deptno, hiredate
+FROM emp
+WHERE empno > 7900 OR empno < 7600
+ORDER BY empno ASC;
 
 -- End of file
