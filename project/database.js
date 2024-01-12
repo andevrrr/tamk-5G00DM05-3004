5ea27cd1fbe81c7297a5e690223b2358bcd0b047
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-12 18:19:58 +0300

    project/database.js: establishing database connection

---
+++ project/database.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+const DB_PATH = './database.db';
+
+let db = new sqlite3.Database(DB_PATH, (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
+
+module.exports = db;
