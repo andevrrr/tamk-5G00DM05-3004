e552b34a1ad5fe195d6d2249f0a996f5c43457e5
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-13 00:38:39 +0300

    project/sql/create.sql: added UNIQUE identifiers

--- project/sql/create.sql
+++ project/sql/create.sql
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS assignments (
   course_id INTEGER NOT NULL,
   student_id INTEGER NOT NULL,
   FOREIGN KEY (course_id) REFERENCES courses(id),
-  FOREIGN KEY (student_id) REFERENCES students(id)
+  FOREIGN KEY (student_id) REFERENCES students(id),
+  UNIQUE(student_id, course_id)
 );
 
 CREATE TABLE IF NOT EXISTS enrollments (
@@ -25,5 +26,6 @@ CREATE TABLE IF NOT EXISTS enrollments (
   student_id INTEGER NOT NULL,
   course_id INTEGER NOT NULL,
   FOREIGN KEY (student_id) REFERENCES students(id),
-  FOREIGN KEY (course_id) REFERENCES courses(id)
+  FOREIGN KEY (course_id) REFERENCES courses(id),
+  UNIQUE(student_id, course_id)
 );
