acb94359a113543f98a161ce20ef8a47b75dd45c
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-13 17:00:35 +0300

    project/server.js: removed hellow word res.send

--- project/server.js
+++ project/server.js
@@ -60,10 +60,6 @@ runSqlFile(createSqlFile);
 //     }
 // });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
